6a2916960a8ac4de66f9fcaf5f1c4e04a2449d0a
Pohe Talu <sagsimadata@gmail.com>
2026-01-30 08:44:39 +0700

    Delete pc-server directory

--- pc-server/install.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Installing Python dependencies...
-python -m pip install -r requirements.txt
-echo.
-echo Installation complete!
-echo.
-echo To run the server: python server.py
-pause
--- pc-server/run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Starting Android Extended Screen Server...
-python server.py
-pause
